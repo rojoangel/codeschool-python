d6da585a9524345c651bb0ee7cf368d9396041cc
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 20:55:15 +0100

    A Function for Lottery Numbers

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -1,15 +1,7 @@
 import random
 
-num = random.randint(1,10)
-
-guess = int(input('Guess a number between 1 and 10'))
-times = 1
-while guess != num:
-    if times == 3:
-        break
-    guess = int(input('Guess again'))
-    times = times + 1
-if guess == num: 
-    print('You win!')
-else:
-    print('You lose! The number was ', num)
+def lotto_numbers():
+    lotto_nums = []
+    for i in range(5):
+        lotto_nums.append(random.randint(1, 53))
+    return lotto_nums
