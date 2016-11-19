698a76612acb29db20b62811a15c53f295612c6c
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 21:57:42 +0100

    Creating a Module for Weather

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -1,10 +1,6 @@
-import requests
-  
-url = "http://api.openweathermap.org/data/2.5/weather?q=Orlando,fl&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0"
-request = requests.get(url)
+import weather
 
-weather_json = request.json()
-print(weather_json)
-weather_main = weather_json['main']
-temp = weather_main['temp']
-print("The Circus's current temperature is", temp)
+def main():
+    weather.current_weather()
+
+main()
