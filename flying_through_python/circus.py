41f16d264f2c4e9bddd3c3e66bb1be49a02fe793
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 21:30:19 +0100

    Reading schedule.txt

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -1,9 +1,4 @@
-performances = {'Ventriloquism':       '9:00am', 
-                'Snake Charmer':       '12:00pm', 
-                'Amazing Acrobatics':  '2:00pm', 
-                'Enchanted Elephants': '5:00pm'}
-
-schedule_file = open('schedule.txt', 'w')
-for key, val in performances.items():
-    schedule_file.write(key + ' - ' + val + '\n')
+schedule_file = open('schedule.txt', 'r')
+for line in schedule_file:
+    print(line)
 schedule_file.close()
