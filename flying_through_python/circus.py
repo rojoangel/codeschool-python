1ded84c5464cd970a39708fe490a678a516bf443
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 21:35:19 +0100

    Reading Into a Dictionary

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -1,5 +1,10 @@
+performances = {}
+
 schedule_file = open('schedule.txt', 'r')
+
 for line in schedule_file:
-    (show, time) = line.split(' - ')  
-    print(show, time)
-schedule_file.close()
+    (show, time) = line.split(' - ')
+    performances[show] = time.strip()
+
+schedule_file.close() 
+print(performances)
