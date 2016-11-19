e3d03aec906aa00d98261ede22ce9d173b65bd8b
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 13:10:13 +0100

    Removing List Items

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -1,3 +1,5 @@
-performances = ['Ventriloquism', 'Amazing Acrobatics']
-performances.append('Snake Charmer')
+performances = ['Ventriloquism', 'Amazing Acrobatics', 'Snake Charmer', 'Enchanted Elephants', 'Bearded Lady', 'Tiniest Man']
+# performances.append('Snake Charmer')
+performances.remove('Bearded Lady')
+performances.remove('Tiniest Man')
 print(performances)
