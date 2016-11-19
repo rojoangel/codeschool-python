4e6877dbbbfa0d2ca07803bd0fafad41ab2a4d5d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 21:09:15 +0100

    Guess the Number Function

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -1,5 +1,21 @@
 import random
 
+def guessing_game():
+    num = random.randint(1, 10)
+    guess = int(input('Guess a number between 1 and 10'))
+    times = 1
+
+    while guess != num:
+        guess = int(input('Guess again'))
+        times += 1
+        if times == 3:
+            break
+
+    if guess == num:
+	      print('You win!')
+    else:
+        print('You lose! The number was', num)
+
 def lotto_numbers():
     lotto_nums = []
     for i in range(5):
@@ -10,5 +26,6 @@ def lotto_numbers():
 def main():
     numbers = lotto_numbers()
     print(numbers)
-
-main()    
+    guessing_game()
+    
+main()
