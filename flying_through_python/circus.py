304c6b8d21727208b1b8d7d684bb56621969a228
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 20:47:38 +0100

    Guess the Number Game (Improved)

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -1,7 +1,15 @@
 import random
 
 num = random.randint(1,10)
+
 guess = int(input('Guess a number between 1 and 10'))
+times = 1
 while guess != num:
+    if times == 3:
+        break
     guess = int(input('Guess again'))
-print('You win!') 
+    times = times + 1
+if guess == num: 
+    print('You win!')
+else:
+    print('You lose! The number was ', num)
