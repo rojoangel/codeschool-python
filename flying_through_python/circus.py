9b02596add2b4bc15bf6d0a2a9e72e46ba6d4c91
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 21:01:11 +0100

    Adding main()

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -3,8 +3,12 @@ import random
 def lotto_numbers():
     lotto_nums = []
     for i in range(5):
-        lotto_nums.append(random.randint(1, 53))      
+        lotto_nums.append(random.randint(1, 53)) 
+    
     return lotto_nums
+  
+def main():
+    numbers = lotto_numbers()
+    print(numbers)
 
-numbers = lotto_numbers()
-print(numbers)
+main()    
