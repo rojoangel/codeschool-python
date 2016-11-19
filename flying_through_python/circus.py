c70f50c800a258b182a71b5067d829aee0a96abb
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 20:41:35 +0100

    Guess the Number Game

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -1,6 +1,7 @@
-performances = {'Ventriloquism':'9:00am', 
-                'Snake Charmer': '12:00pm', 
-                'Amazing Acrobatics': '2:00pm', 
-                'Enchanted Elephants':'5:00pm'}
-for name, showtime in performances.items():
-    print(name, ': ', showtime, sep='')
+import random
+
+num = random.randint(1,10)
+guess = int(input('Guess a number between 1 and 10'))
+while guess != num:
+    guess = int(input('Guess again'))
+print('You win!') 
