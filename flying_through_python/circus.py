722616c6cb0b1537c2075debb0bfb59af8867472
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 20:56:30 +0100

    Calling Our Function

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -3,5 +3,8 @@ import random
 def lotto_numbers():
     lotto_nums = []
     for i in range(5):
-        lotto_nums.append(random.randint(1, 53))
+        lotto_nums.append(random.randint(1, 53))      
     return lotto_nums
+
+numbers = lotto_numbers()
+print(numbers)
