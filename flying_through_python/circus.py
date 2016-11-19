dae6e90b00e5ba37a0ee697700d22093adce31de
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-11-19 21:26:14 +0100

    Writing the Circus Schedule

--- flying_through_python/circus.py
+++ flying_through_python/circus.py
@@ -1,39 +1,9 @@
-import random
+performances = {'Ventriloquism':       '9:00am', 
+                'Snake Charmer':       '12:00pm', 
+                'Amazing Acrobatics':  '2:00pm', 
+                'Enchanted Elephants': '5:00pm'}
 
-def guessing_game():
-    num = random.randint(1, 10)
-
-    guess = int(input('Guess a number between 1 and 10'))
-    times = 1
-
-    while guess != num:
-        guess = int(input('Guess again'))
-        times += 1
-        if times == 3:
-            break
-
-    if guess == num:
-        print('You win!')
-    else:
-        print('You lose! The number was', num)
-    
-
-def lotto_numbers():
-    lotto_nums = []
-    for i in range(5):
-        lotto_nums.append(random.randint(1, 53)) 
-    
-    return lotto_nums
-  
-def main():
-    answer = input ('Do you want to get lottery numbers (1) or play the game (2) or quit (Q)?')
-    
-    if answer == '1':
-        numbers = lotto_numbers()
-        print(numbers)
-    elif answer == '2':
-        guessing_game()
-    else:
-        print('Toodles!')
-    
-main()
+schedule_file = open('schedule.txt', 'w')
+for key, val in performances.items():
+    schedule_file.write(key + ' - ' + val + '\n')
+schedule_file.close()
